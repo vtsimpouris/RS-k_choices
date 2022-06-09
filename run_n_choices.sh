7cb68f36554fdabc1ae1ad5cf70ce7b60ca8c00d
vtsimpouris <43888506+vtsimpouris@users.noreply.github.com>
2022-06-09 12:32:01 +0200

    Add files via upload

---
+++ run_n_choices.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+./example 1
+./example 2
+./example 3
+./example 4
+./example 5
+./example 6
+./example 7
+./example 8
+./example 9
+./example 10
+./example 11
+./example 12
+./example 13
+./example 14
+./example 15
+./example 16
