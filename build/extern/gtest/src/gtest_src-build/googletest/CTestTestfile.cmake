672adc2ac6a66cce57358a5ea173879cee966e86
vtsimpouris <43888506+vtsimpouris@users.noreply.github.com>
2022-06-09 12:32:29 +0200

    Delete build/extern/gtest directory

--- build/extern/gtest/src/gtest_src-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vtsimpouris/Desktop/RS_n_choices/build/extern/gtest/src/gtest_src/googletest
-# Build directory: /home/vtsimpouris/Desktop/RS_n_choices/build/extern/gtest/src/gtest_src-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
